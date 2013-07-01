1760dd8de8a19a4e000d5adc06a9a66d871f8cf9
Bill Keese <bill@dojotoolkit.org>
2013-07-02 08:20:50 +0900

    rename appConfig.js to config.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ grunt.registerTask('travis', 'jshint');
   grunt.initConfig({
     jshint: {
       // define the files to lint
-      files: ['Guntfile.js', 'app.js', 'spider.js', 'appConfig.js', 'lib/**/*.js', 'test/**/*.js', 'public/scripts/api.js', 'public/scripts/api/*.js'],
+      files: ['Guntfile.js', 'app.js', 'spider.js', 'config.js', 'lib/**/*.js', 'test/**/*.js', 'public/scripts/api.js', 'public/scripts/api/*.js'],
       jshintrc : "jshint.config",
       // configure JSHint (documented at http://www.jshint.com/docs/)
       options: {jshintrc: 'jshint.config'}
