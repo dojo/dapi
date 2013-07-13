752119d8d63efd186a9adb7e352e7138f10a3f0d
lbod <leebodzak@hotmail.com>
2013-07-14 00:40:49 +0100

    module contentpanes and accordions added, icons and minor cleanup

--- public/scripts/mobileapi.js
+++ public/scripts/mobileapi.js
@@ -15,18 +15,22 @@ require([
     "dojox/mobile/ViewController",
     "dojox/mobile/ScrollableView",
     "dojo/dom-style",
-    "dojox/mobile/bookmarkable"
-], function (parser, dom, lang, ready, config, on, Heading, View, RoundRectList, RoundRect, ListItem, registry, request, ViewController, ScrollableView, domStyle, bookmarkable) {
+    "dojox/mobile/bookmarkable",
+    "dojo/has",
+    "dojox/mobile/ContentPane",
+    "dojox/mobile/Accordion"
+], function (parser, dom, lang, ready, config, on, Heading, View, RoundRectList, RoundRect, ListItem, registry, request, ViewController, ScrollableView, domStyle, bookmarkable, has, ContentPane, Accordion) {
     var moduleModel = null, moduleTree = null, currentVersion = null, apiSearchToolTipDialog = null, apiSearchWidget = null, vc = ViewController.getInstance(), treesdataview = null, mainview = null;
     ready(function () {
         domStyle.set(document.body, "display", "block"); // naff but prevents the FOUC (body.display set to none in markup)
+        new Accordion();// TODO : test if it's specific to Accordion i.e. by loading an instance first - this works if you load another Accordion module xhr pane first
         var parsed = parser.parse();
         treesdataview = registry.byId("treesdataview");
         mainview = registry.byId("mainview");
-        console.warn("%cThis DOJO mobile API viewer is just a proof of concept and not fully functional", "color:black; background-color:red; font-size:14px");
-        console.warn("---- %cIt's aim is to show extending the API viewer to show an alternate presentation. Configure the views directory to mobileviews to see it in action", "text-decoration:underline;");
+        console.log("%cSee dojox/mobile/Accordion for the xhr and contentpane problem; this doesn't happen if you load any non listitem'd module pane first.  Also click on the childwidget  LI and that's loaded into the same pane.", "color:black; background-color:red; font-size:14px");
+        console.warn("----> %cThis DOJO mobile API viewer is just a proof of concept and not fully functional", "color:black; background-color:red; font-size:14px");
+        console.warn("----> %cIt's aim is to show extending the API viewer to show an alternate presentation. Configure the views directory to mobileviews to see it in action", "text-decoration:underline;");
 
-        console.log("Will use all ECMA6 features like querySelectorAll/forEach etc therefore I won't work in browsers that don't support me and I won't be using the compat package");
         // don't use query
         // query vars: 
         var versionlistitems = document.querySelectorAll("#versionlist li");
@@ -45,183 +49,91 @@ require([
                     buildTreeView(data, "", "", version);
                     // mainview.performTransition(treesdataview);
                     var versionrenamed = data.version.replace(/\./, '_');
-                    versionlistlink.transitionTo("treesdataview_" + versionrenamed);
+                    versionlistlink.transitionTo("treesdataview_" + version);
                 }, function (err) {
                     alert(err);
                 });
                 console.log(ev);
             });
         }
-/*        
-        apiSearchToolTipDialog = registry.byId("apiSearchToolTipDialog");
-        apiSearchToolTipDialog.closable = true;
-        s.onchange = lang.hitch(s, versionChange);
-        buildTree();
-// selectAndClick setup the welcome page (selectAndClick is defined by buildTree)
-        var welcomeTab = registry.byId("baseTab_welcomeTab");
-        query(".dtk-object-title a", welcomeTab.domNode).forEach(function (node, index) {
-            on(node, "click", function (e) {
-                console.log(e.target.innerHTML);
-                var targetpatharr = e.target.name.split("/"), treepatharr = [], tmp2 = null;
-                // TODO : do this better, filter/map?
-                // builds an array of paths for a tree (must be in this order) -> an ending slash (empty i.e. a folder) -> a module path to but not the module i.e.charting in dojox/charting/Chart (idx < arr.length -1) -> and the module name (the last item)   
-                array.forEach(targetpatharr, function (item, idx, arr) {
-                    if (arr[idx] === "") {
-                    } else if (idx < arr.length - 1) {
-                        var tmp2 = (arr.slice(0, idx + 1).join("/")  + "/");
-                        treepatharr.push(tmp2);
-                    } else {
-                        treepatharr.push(arr.slice(0, idx + 1).join("/"));
-                    }
-                });
-                moduleTree.selectAndClick(treepatharr);
-                e.preventDefault();
-            });
-        });
-// end selectAndClick
 
-        // TODO - syntax highlighter for premalinked loaded modules -- plus this should be reusable (mixin?) as its also used in moduletree.js
-        // should do as i thought, create an extended ContentPane with these functions because the show/hide semantics needs to be captured too (for summaries)
-        var content = dom.byId("content");
-        var contentpane = registry.byId("baseTab"); // the default loaded module tab (even when there is a permalink and intro screen) will always be named baseTab - intro screen id changed
-        if (contentpane) {
-            contentpane.initModulePane();
-        }
-*/
-
-/*
-        var tabContainer = registry.byId("content");
-        // temp - add a close all option - move to context menu on the tab label
-        dojo.getObject("dijit.layout._ScrollingTabControllerMenuButton").prototype.loadDropDown = function (callback) {
-            this.dropDown = new Menu({
-                id: this.containerId + "_menu",
-                ownerDocument: this.ownerDocument,
-                dir: this.dir,
-                lang: this.lang,
-                textDir: this.textDir
-            });
-            var container = registry.byId(this.containerId);
-            // add close all
-            var menuItem = new MenuItem({
-                label: "Close all",
-                iconClass: "dijitInline dijitIcon dijitMenuItemIcon dijitIconDelete",
-                onClick: function () {
-                    var _this = this;
-                    tabContainer.getChildren().forEach(function (item) {
-                        console.log(item);
-                        if (item.closable) {
-                            tabContainer.removeChild(item);
-                            item.destroyRecursive();
-                        }
-                    });
-                }
-            });
-            this.dropDown.addChild(menuItem);
-            this.dropDown.addChild(new MenuSeparator());
-            // end close all            
-
-            array.forEach(container.getChildren(), function (page) {
-                var menuItem = new MenuItem({
-                    id: page.id + "_stcMi",
-                    label: page.title,
-                    iconClass: page.iconClass,
-                    disabled: page.disabled,
-                    ownerDocument: this.ownerDocument,
-                    dir: page.dir,
-                    lang: page.lang,
-                    textDir: page.textDir,
-                    onClick: function () {
-                        container.selectChild(page);
-                    }
-                });
-                this.dropDown.addChild(menuItem);
-            }, this);
-            callback();
-        };
-        // end temp - add a close all option - move to context menu on the tab label
-*/
     });
 
 
-    var buildTreeView = function (data, path, objtype, version) {
+    var buildTreeView = function (dataitem, path, objtype, version) {
         //var treedatalist = registry.byId("treedatalist"), treecontainernode = treedatalist.containerNode;
-        var treeitem = null, children = data.children;
-        var viewsfromto = getTreeViewsToTransition(path, data, version);
+        var treeitem = null, children = dataitem.children;
+        var viewsfromto = getTreeViewsToTransition(dataitem, path, dataitem, version);
 
         //treedatalist.destroyDescendants();
         //console.log(treesdataview);
         // we're not root and we've been sent a path that looks like folder/dijit/Tree  (folder|object|function etc) 
-
-/*
-        if (path && path.length > 0 && djtype && djtype !== "object" && djtype !== "function") {
-            var tmparr = data.children.filter(function (item) {
-                var pathsplit = path.split("/");
-                //.replace(/[^\/]*./, '');
-                var objtype = pathsplit[0];
-                var pathfolder = pathsplit.splice(1, pathsplit.length).join("/");
-                console.log(objtype, pathfolder);
-
-                return (item.fullname === pathfolder);
-                //return (item.fullname == path.replace(\^/\\, "")) ;
-            });
-            children = tmparr[0] && tmparr[0].children ? tmparr[0].children : children;
-        } else if (djtype === "object" || djtype === "function"){
-            console.log(path, djtype);
-        } else {
-            // empty path is the root folder - change crap logic later
-            children = data.children;
+        if (!dataitem.children ||  dataitem.children &&  dataitem.children > 0) {
+            return;
         }
-*/
+        var roundlist = new RoundRectList();
+        roundlist.startup();
         children.forEach(function (item, idx, arr) {
-            //var id = data.version + '/' + item.name;
+            //var id = dataitem.version + '/' + item.name;
             // TODO ignore objects which are expandable i.e. they likely have a item.type = 'object' (as compared to item.type = 'folder')
             var itemtype = item.type;
             //console.log(item.type, item.name);
 
             // object|constructor|folder all seem to be able to have children
-            console.log("name = " + item.fullname + ", type = " + item.type + ", hasChildren = " + (item.children && item.children.length > 0 ? "true" : "false"));
+            //console.log("name = " + item.fullname + ", type = " + item.type + ", hasChildren = " + (item.children && item.children.length > 0 ? "true" : "false"));
 
             // constructor|object|folder|function|instance|undefined(this isn't right "dijit/robot") 
             // only folder will have no associated view - also test for children to add a new list
-            if (itemtype === 'folder') {
+            if (itemtype !== 'folder' && item.children && item.children.length > 0) {
+                console.warn(itemtype + ", item name = " + item.name + ", children.length = " + item.children.length);
                 //return;
             }
-            treeitem = new ListItem({label : item.name, moveTo : '#', id : itemtype + "/" + item.fullname});
+            var rightIcon2 = (item.type === "folder" ? "mblDomButtonSilverCircleDownArrow" : "mblDomButtonBlueCircleArrow"); // 
+            var rightText = (item.type === "folder" ? "Expand" : "View"); //
+            treeitem = new ListItem({label : item.name + (item.type === "folder" ? "    >" : ""), moveTo : '#', id : itemtype + "/" + item.fullname, noArrow: true, rightIcon2 : rightIcon2, rightText : rightText});
             treeitem.startup();
+            roundlist.addChild(treeitem);
             //treeitem.placeAt(treecontainernode);
-            treeitem.placeAt(viewsfromto.to.containerNode);
+            //treeitem.placeAt(viewsfromto.to.containerNode);
+            roundlist.placeAt(viewsfromto.to.containerNode);
 
             treeitem.on("click", function (ev) {
                 var linkto = this.id;
-                var viewsfromtoinner = getTreeViewsToTransition(linkto, data, version);
+                var viewsfromtoinner = getTreeViewsToTransition(item, linkto, dataitem, version);
                 var pathsplit = linkto.split("/");
                 //.replace(/[^\/]*./, '');
                 var objtype = pathsplit[0];
                 //var pathfolder = pathsplit.splice(1, pathsplit.length).join("/");
 
                 console.log(linkto, this.type);
-                if (item.type === 'folder') {
+                //if (item.type === 'folder') { // 12/07 temp stub
+                if (true) {
                     buildTreeView(item, linkto, objtype, version);
                     //viewsfromto.to.destroyDescendants();
                     viewsfromtoinner.from.performTransition(viewsfromtoinner.to.id, 1, "slide");
                     console.log(viewsfromtoinner);
                     //treeitem.transitionTo("treesdataview");
                 } else {
-                    showModuleView(item, linkto, objtype);
+                    //viewsfromtoinner.to    
+                    createModulePane(item, linkto, objtype, version);
                 }
                 ev.preventDefault();
             });
+            // end on
+
+
         });
     };
 
     // TODO : use this to get the from and to views to transition to, Return an object {from:view, to:view} so transitions can be performed, based on the level in the tree i.e. dijit (1), dijit/form (2)
-    var getTreeViewsToTransition = function (path, data, version) {
+    var getTreeViewsToTransition = function (item, path, data, version) {
         var scrollableParams = {fixed : "top"};
         var obj = {};
         obj.to = "";
         obj.from = "";
         var toview = null;
-        var versionformatted = version.replace(/\./, '_');
+        //var versionformatted = version.replace(/\./, '_');
+        var versionformatted = version;
         if (path === "") {
             obj.from = registry.byId("mainview");
             obj.to = registry.byId("treesdataview_" + versionformatted);
@@ -235,62 +147,73 @@ require([
                 obj.from = registry.byId("treesdataview_" + versionformatted);
                 toview = registry.byId(toviewid);
                 if (!toview) {
-                    toview = new ScrollableView({id: toviewid, keepScrollPos: false});
-                    toview.placeAt(document.body);
-                    var heading = new Heading({
-                        label: toviewid + " " + version,
-                        back : "back",
-                        fixed : "top",
-                        moveTo : "treesdataview_" + versionformatted
-                    });
-                    toview.addFixedBar(heading);
-                    toview.startup();
+                    // id, version, label, moveTo
+                    toview = createView(item, toviewid, versionformatted, toviewid + " " + version, "treesdataview_" + versionformatted);
                 }
                 obj.to = toview;
             } else {
                 toview = registry.byId(toviewid);
                 pathsplit.pop(); // remove the last element i.e. this will be the parent of the object i.e. dijit/form/_FormSelectWidget will be dijit/form
-                var fromviewid = pathsplit.join("/");
-                var fromview =  registry.byId(fromviewid);
+                var fromviewid = pathsplit.join("/"), fromview =  registry.byId(fromviewid);
 
                 if (!fromview) {
-                    fromview = new ScrollableView({id: fromviewid, keepScrollPos: false});
-                    fromview.placeAt(document.body);
-                    var headingfrom = new Heading({
-                        label: fromviewid + " " + version,
-                        fixed : "top",
-                        back : "back"
-                    });
-                    fromview.addFixedBar(headingfrom);
-
-                    fromview.startup();
+                    fromview = createView(item, fromview, versionformatted, fromview + " " + version);
                 }
-
-
                 if (!toview) {
-                    toview = new ScrollableView({id: toviewid, keepScrollPos: false});
-                    toview.placeAt(document.body);
-                    var headingto = new Heading({
-                        label: toviewid + " " + version,
-                        back : "back",
-                        fixed : "top",
-                        moveTo : fromview.id
-                    });
-                    toview.addFixedBar(headingto);
-                    toview.startup();
+                    toview = createView(item, toviewid, versionformatted, toviewid + " " + version, fromview.id);
                 }
                 obj.from = fromview;
                 obj.to = toview;
             }
-//            obj.from = registry.byId("treesdataview" + pathsplit.length);
-//            obj.to = registry.byId("treesdataview" + (pathsplit.length + 1));
-            //console.log(obj);
         }
         return obj;
     };
 
-    var showModuleView = function (item, linkto, objtype) {
+
+    // TODO: use this to get the object details from the id i.e. version/objecttype/modulepath 
+    var getObjectId = function (path) {
+        var pathsplit = path.split("/"), version = pathsplit.pop(), objectType = pathsplit.pop();
+        return {version : version, objectType : objectType, module : pathsplit.join("/")};
+    };
+
+    // TODO:  encapsulate view creation
+    var createView = function (item, id, version, label, moveTo) {
+        var view = null, headingobj = null, heading = null, contentpane;
+        //view = new ScrollableView({id: id, keepScrollPos: false, scrollType:2});
+        view = new View({id: id, keepScrollPos: false, scrollType: 2});
+
+        headingobj = {label : id + " " + version, fixed : "top", back : "back"};
+        if (moveTo) {
+            headingobj.moveTo = moveTo;
+        }
+        view.placeAt(document.body);
+        heading = new Heading(headingobj);
+        //view.addFixedBar(heading);
+        view.addChild(heading);
+
+        if (item.type !== 'folder') {
+            contentpane = createModulePane(item, null, null, version);
+            view.addChild(contentpane);
+        }
+
+        view.startup();
+        return view;
+    };
+
+    // TODO: use to show a module i.e. xhr
+    var createModulePane = function (item, linkto, objtype, version) {
         console.log(item, linkto, objtype);
+        var href = "/api/" + version + "/" + item.fullname + ".html";
+        //var contentpane = new ContentPane({href:href});
+        var contentpane = new ContentPane();
+        //contentpane.placeAt(document.body);
+        on(contentpane, "load", function () {
+            console.log("href=" + href + " loaded");
+        });
+
+        contentpane.set("href", href);
+        return contentpane;
+
     };
 
     var buildModuleView = function (data) {
