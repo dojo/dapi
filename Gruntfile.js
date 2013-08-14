af5d57b0c1619b7224bf0a0a276a18f67397d858
lbod <leebodzak@hotmail.com>
2013-08-14 20:25:08 +0100

    update grunt for devTasks; remove api data (why is it still in the repo?)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,23 @@
 module.exports = function(grunt) {
-//grunt.registerTask('default', ['jshint']);
-grunt.loadNpmTasks('grunt-contrib-jshint');
-grunt.registerTask('default', ['jshint']);
-grunt.registerTask('travis', 'jshint');
-
-  grunt.initConfig({
-    jshint: {
-      // define the files to lint
-      files: ['Guntfile.js', 'app.js', 'spider.js', 'config.js', 'lib/**/*.js', 'test/**/*.js', 'public/scripts/api.js', 'public/scripts/api/*.js'],
-      // configure JSHint (documented at http://www.jshint.com/docs/)
-      options: {jshintrc: '.jshintrc'}
-      }
-   });
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('travis', 'jshint');
+    grunt.registerTask('devTasks', ['shell']); // i.e. grunt devTasks:installAPIExampleData
+    grunt.initConfig({
+        jshint: {
+            // define the files to lint
+            files: ['Guntfile.js', 'app.js', 'spider.js', 'config.js', 'lib/**/*.js', 'test/**/*.js', 'public/scripts/api.js', 'public/scripts/api/*.js'],
+            // configure JSHint (documented at http://www.jshint.com/docs/)
+            options: {jshintrc: '.jshintrc'}
+        },
+        shell: {                                // Task
+            installAPIExampleData: {                   // Target
+                options: {                      // Options
+                    stdout: true
+                },
+                command: 'git clone https://github.com/lbod/dojo-api-data.git ./public/data'
+            }
+        }
+    });
 };
